ddc810f2088cb2d69b08213fdcb237dac7a45711
artem <artem@COMPUTER>
2022-08-26 11:50:49 +0300

    Добавьте файлы проекта.

---
+++ triangleDraw/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace triangleDraw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        Polygon triangle = new Polygon();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(Color.White);
+            triangle.generate();
+            triangle.draw(g);
+        }
+    }
+}
---
+++ triangleDraw/Polygon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangleDraw
+{
+    public partial class Polygon
+    {
+        public PointF[] points { get; private set; } = new PointF[3];
+        public dynamic[] colors = new dynamic[4];
+        public dynamic brush;
+        public static int width = 10;
+        public Pen pen = new Pen(Color.Black, width);
+        
+        
+        public Polygon()
+        {
+            this.colors[0] = Brushes.Black;
+            this.colors[1] = Brushes.Red;
+            this.colors[2] = Brushes.Green;
+            this.colors[3] = Brushes.Blue;
+        }
+        
+        public void generate()
+        {
+            Random rand = new Random();
+            this.brush = colors[rand.Next(0, 4)];
+            for (int i = 0; i < 3; i++)
+            {
+                this.points[i] = new PointF((float)rand.Next(200, 800), (float)rand.Next(200, 400));
+            }
+            
+        }
+        
+        public void draw(Graphics g)
+        {
+
+            g.FillPolygon(brush, points);
+        }
+    }
+}
